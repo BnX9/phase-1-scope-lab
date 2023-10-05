c5c071990ca4deee894dfc03712638a291bbe5a2
Brian Ngugi <brianngugi.sw@gmail.com>
2023-10-05 11:47:45 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+  const capital = customerName.toUpperCase()
+  customerName = capital
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+
+const leastFavouriteCustomer = 'bob'
+function changeLeastFavoriteCustomer(){
+  leastFavouriteCustomer = 'jim'
+}
